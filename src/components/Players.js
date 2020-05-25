a96f9eddb1c8a6c996bf182d1f353583db970ed7
stangalla <stangalla@miraclesoft.com>
2020-05-25 13:21:11 -0400

    Rendering all players

--- src/components/Players.js
+++ src/components/Players.js
@@ -40,32 +40,29 @@ export default class Players extends Component {
           ? <div className='sidebar-instruction'>Select a Player</div>
           : null}
 
-        <Route path={`${match.url}/:playerId`} render={({ match }) => {
+        <Route path={`${match.url}/:playerId`} render={({match}) => {
+          if (loading === true) return null
 
-          if ( loading === true) 
+          const {
+            name, position, teamId, number, avatar, apg, ppg, rpg, spg
+          } = players.find((player) => slug(player.name) === match.params.playerId)
 
-            return null
-
-            const { 
-              name, position, teamId, number, avatar, apg, ppg, rpg, spg,
-            } = players.find((player) => slug(player.name) === match.params.playerId)
-
-            return(
-              <div className='panel'>
+          return (
+            <div className='panel'>
               <img className='avatar' src={`${avatar}`} alt={`${name}'s avatar`} />
               <h1 className='medium-header'>{name}</h1>
               <h3 className='header'>#{number}</h3>
               <div className='row'>
                 <ul className='info-list' style={{marginRight: 80}}>
-                <li>Team 
-                  <div>
-                    <Link style={{color: '#68809a'}} to={`/${teamId}`}>
-                      {teamId[0].toUpperCase()+ teamId.slice(1) }
-                    </Link>
-                  </div>
-                </li>
-                <li>Position<div>{position}</div> </li>
-                <li>PPG<div>{ppg}</div> </li>
+                  <li>Team
+                    <div>
+                      <Link style={{color: '#68809a'}} to={`/${teamId}`}>
+                        {teamId[0].toUpperCase() + teamId.slice(1)}
+                      </Link>
+                    </div>
+                  </li>
+                  <li>Position<div>{position}</div></li>
+                  <li>PPG<div>{ppg}</div></li>
                 </ul>
                 <ul className='info-list'>
                   <li>APG<div>{apg}</div></li>
@@ -73,11 +70,9 @@ export default class Players extends Component {
                   <li>RPG<div>{rpg}</div></li>
                 </ul>
               </div>
-              </div>
-            )
-
-        }}/>
-
+            </div>
+          )
+        }} />
       </div>
     )
   }
