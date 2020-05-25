f43e982d7ea64259d96cf8075aa82270608bb14b
stangalla <stangalla@miraclesoft.com>
2020-05-25 14:04:11 -0400

    Fetching the teams

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,12 +1,49 @@
 import React, { Component  } from 'react'
+import Sidebar from './Sidebar'
+import { getTeamNames } from '../api'
+import { truncate } from 'fs';
 
 export default class Teams extends Component { 
 
+    state = { 
+        teamNames: [],
+        loading: true 
+    } 
+
+    componentDidMount () { 
+        getTeamNames()
+            .then((teamNames) => {
+                this.setState(() => ({
+                  
+                    loading: false,
+                    teamNames,
+                }))
+            })
+
+
+    }
+ 
     render() { 
+
+        const { loading, teamNames } = this.state
+        const { location, match } = this.props
         return (
 
-            <div className='container'> 
-            Teams
+            <div className='container two-column'> 
+            
+            <Sidebar 
+                loading={loading}
+                title='Teams'
+                list={teamNames}
+                {...this.props}
+
+                />
+
+     {loading === false && location.pathname === '/teams'
+          ? <div className='sidebar-instruction'>Select a Team</div>
+          : null}
+
+
             </div>
         )
     }
