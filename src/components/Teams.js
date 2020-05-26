d2df1b68a879fb72997fbbf53ab46dc8b3baed40
stangalla <stangalla@miraclesoft.com>
2020-05-25 21:03:01 -0400

    Rendering the teams

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -2,6 +2,11 @@ import React, { Component  } from 'react'
 import Sidebar from './Sidebar'
 import { getTeamNames } from '../api'
 import { truncate } from 'fs';
+import {Route, Link } from 'react-router-dom'
+import TeamLogo from './TeamLogo'
+import Team from './Team'
+
+
 
 export default class Teams extends Component { 
 
@@ -43,7 +48,31 @@ export default class Teams extends Component {
           ? <div className='sidebar-instruction'>Select a Team</div>
           : null}
 
+        <Route path={`${match.url}/:teamId`} render={({ match }) => (
 
+            <div className='panel'>
+            
+            <Team id={match.params.teamId}>
+
+               {(team) => team === null ? <h1>LOADING </h1>: <div style={{width: '100%'}}><TeamLogo id={team.id} className='center' />
+               <h1 className='medium-header'>{team.name}</h1>
+               <ul className='info-list row'>
+                <li>Established<div>{team.established}</div></li>
+                <li>Manager<div>{team.manager}</div></li>
+                <li>Coach<div>{team.coach}</div></li>
+               </ul>
+
+               <Link className='center btn-main' to={`/${match.params.teamId}`}>
+               {team.name} Team Page
+               </Link>
+               </div> }
+
+            </Team>
+
+            
+
+            </div>
+        )}/>
             </div>
         )
     }
