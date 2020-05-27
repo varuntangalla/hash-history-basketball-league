bc50c7c5a9d4addb86e728475b86c3c559e53723
stangalla <stangalla@miraclesoft.com>
2020-05-27 15:19:21 -0400

    Updates

---
+++ precache-manifest.cb646f9678d2cca8e4f198a008e67dce.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119d74d75d20efbf7a456a67cb336047",
+    "url": "/hash-history-basketball-league/index.html"
+  },
+  {
+    "revision": "e04038715bb4922bac3a",
+    "url": "/hash-history-basketball-league/static/css/main.35d2342b.chunk.css"
+  },
+  {
+    "revision": "5842f61fb9ab4efd01a3",
+    "url": "/hash-history-basketball-league/static/js/0.3c2fdb0b.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/hash-history-basketball-league/static/js/0.3c2fdb0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "737f63860b5427c417dd",
+    "url": "/hash-history-basketball-league/static/js/1.790f4161.chunk.js"
+  },
+  {
+    "revision": "ddc6cf75d847cb36ecca",
+    "url": "/hash-history-basketball-league/static/js/10.edb1c711.chunk.js"
+  },
+  {
+    "revision": "0afc3eb1bb3b2111631a",
+    "url": "/hash-history-basketball-league/static/js/11.067f0a4a.chunk.js"
+  },
+  {
+    "revision": "1b4edf109ac80ee72cd8",
+    "url": "/hash-history-basketball-league/static/js/2.5290db64.chunk.js"
+  },
+  {
+    "revision": "ce2c4ae656978c4c1143",
+    "url": "/hash-history-basketball-league/static/js/5.be55b10e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hash-history-basketball-league/static/js/5.be55b10e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54be2f349086203b6b0",
+    "url": "/hash-history-basketball-league/static/js/6.77fabafa.chunk.js"
+  },
+  {
+    "revision": "731f774b174d54e6bc20",
+    "url": "/hash-history-basketball-league/static/js/7.b7065c57.chunk.js"
+  },
+  {
+    "revision": "54fd4c5be1ea4732e2a1",
+    "url": "/hash-history-basketball-league/static/js/8.86588682.chunk.js"
+  },
+  {
+    "revision": "37615e80ff3d2e97527d",
+    "url": "/hash-history-basketball-league/static/js/9.1ac36c9a.chunk.js"
+  },
+  {
+    "revision": "e04038715bb4922bac3a",
+    "url": "/hash-history-basketball-league/static/js/main.2fb7474c.chunk.js"
+  },
+  {
+    "revision": "8284a021fc70bfe4d49e",
+    "url": "/hash-history-basketball-league/static/js/runtime-main.eaa1eac1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/hash-history-basketball-league/precache-manifest.cb646f9678d2cca8e4f198a008e67dce.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hash-history-basketball-league/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
